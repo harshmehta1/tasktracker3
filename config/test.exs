f070c684e94a61b6d0fbfb1de714ec7c53d572da
harshmehta <harshmehta@ccs.neu.edu>
2018-04-04 23:32:03 -0400

    deploy

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :tasktracker, TasktrackerWeb.Endpoint,
-  http: [port: 4001],
+  http: [port: 5150],
   server: false
 
 # Print only warnings and errors during test
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :tasktracker, Tasktracker.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "tasktracker_test",
+  username: "tasktracker3",
+  password: "watermelon",
+  database: "tasktracker3_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
